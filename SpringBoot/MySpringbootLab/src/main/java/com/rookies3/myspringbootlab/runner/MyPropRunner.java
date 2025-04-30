a61cdabb3196971fb97093cd6cab316b4923ff7a
이도건 <rookies.dogun@gmail.com>
2025-04-30 14:13:57 +0900

    logger 변경

--- SpringBoot/MySpringbootLab/src/main/java/com/rookies3/myspringbootlab/runner/MyPropRunner.java
+++ SpringBoot/MySpringbootLab/src/main/java/com/rookies3/myspringbootlab/runner/MyPropRunner.java
@@ -1,7 +1,6 @@
 package com.rookies3.myspringbootlab.runner;
 
-import lombok.extern.java.Log;
-import org.apache.logging.log4j.Logger;
+import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
@@ -12,8 +11,8 @@ import org.springframework.stereotype.Component;
 
 @Component
 public class MyPropRunner implements ApplicationRunner {
-    private final Logger logger = (Logger) LoggerFactory.getLogger(this.getClass());
 
+    private final Logger logger =  LoggerFactory.getLogger(this.getClass());
 
     @Value("${myprop.username}")
     private String username;
